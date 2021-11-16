28762e5234a51373c306ec30b8e86f155ddfa5d3
EC2 Default User <ec2-user@ip-172-31-39-221.ec2.internal>
2021-11-16 16:42:32 +0000

    mid work save

---
+++ website/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'The website ET-710';
+  dateToday: number = Date.now();
+  
+} 
